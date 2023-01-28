af9133e2be2e5fd2ce64a2de4d326a6a47682461
6StringCode <sixstringcode@gmail.com>
2023-01-27 23:45:08 -0500

    completed app with Add Animal and heart buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ['bird', 'cat', 'cow', 'dog', 'gator', 'horse'];
+
+  return animals[Math.floor(Math.random() * animals.length)]
+}
 
 function App() {
+  const [animals, setAnimals] = useState([]);
+  
+  const handleClick = () => {
+    setAnimals([...animals, getRandomAnimal()])
+  };
+
+  const renderedAnimals = animals.map((animal, idx)=> {
+    return <AnimalShow type={animal} key={idx} />
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* <button onClick={ () => console.log('button was clicked')} > */}
+      <button onClick={handleClick}>Add Animal</button>
+      <div className="animal-list">{renderedAnimals}</div>
     </div>
-  );
+  )
 }
+
+
+
+
+//Replicate with Class Component
+// import React, { Component } from 'react'
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props)
+  
+//     this.state = {
+//        count: 0
+//     }
+//   }
+
+//   handleClick() {
+//     this.setState((prevState, props) => ({
+//       count: prevState + 1
+//     }))
+//     console.log(this.state.count, props)
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={() => this.handleClick()}>Add Animal</button>  
+//         <div>
+//           Number of Animals: {this.state.count}
+//         </div>
+//       </div>
+//     )
+//   }
+// }
 
 export default App;
